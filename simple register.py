d46a1852ff46fb85d1b8e766287c14ef0c7d5915
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-02-24 15:56:19 +0000

    created simple register code ver2

--- simple register.py
+++ simple register.py
@@ -5,28 +5,20 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    def myname():
-        name = input("What is your name?: ")
-        name2 = input("Please type your name again: ")
-
 
     print("1. Register your name")
     name = input("What is your name?: ")
-    name2 = input("Please type your name again: ")
-    if name != name2:
-        print("Try again!")
-        myname()
     print("Hello", name)
-    def password():
-        pw = input("Please typr your password: ")
-        pw2 = input("Type your password again: ")
+
     print("2. Register your password")
-    pw = input("Please type your password: ")
-    pw2 = input("Type your password again: ")
-    if pw != pw2:
-        print("Try again")
-        password()
-    print("Well done!")
+    while True:
+        pw = input("Please type your password: ")
+        pw2 = input("Type your password again: ")
+        if pw != pw2:
+            print("Try again")
+        else:
+            print("Well done!")
+            break
     print("Do you want to see your password? yes or no")
     answer = input("Please type yes or no: ")
     if answer == 'yes':
